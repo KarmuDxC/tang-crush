f1e4d2d527cdf9f66efb186822cb55733628a809
KarmuDxC <88973366+KarmuDxC@users.noreply.github.com>
2021-08-16 20:06:26 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Haluu mẹ!',
+    introDesc: `Mẹ nhớ trả lời hết nha
+    Thoát ra là trả con một ly trà sữa đó.`,
+    btnIntro: 'Ok',
+    title: 'Mẹ có yeww Hân không??',
+    desc: 'Mẹ không yeww hai đứa bằng nhau được đâu MUAHAHA',
+    btnYes: 'Yêu Hân hơn Bum gất là nhìuu',
+    btnNo: 'Mẹ thương cả hai bằng nhau',
+    question:'Tại sao mẹ lại yeww Hân gất nhìu??',
+    btnReply: 'Trả lời thành thật nè',
+    reply: 'Vì Hân quá là xênh đẹp UWU<3333',
+    mess: 'Hehe Hân biết mà 🥰. Yêu mẹ nhiều nhiều 😘😘',
+    messDesc: 'Chúc mẹ sinh nhật vui vẻ nhó. Mẹ nhớ mua chocolate cho Hân.',
+    btnAccept: 'Ok luôn<3',
 }
